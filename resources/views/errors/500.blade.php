749ca4c4147a9f477435d146ab2df32686c54da7
Marek Lovčí <mareklovci@gmail.com>
2020-05-08 19:46:39 +0200

    Issue #8000 Error codes

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="vertical-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <span class="error">500 | Internal Server Error</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
