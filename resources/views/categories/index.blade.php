c74e9df8722790a3e285bc520f49486e7012e5ce
rizir01 <themurrayms@seznam.cz>
2020-05-11 10:38:53 +0200

    Issue #8049 @0.5h
    Zmena scaelovani nazvu Choose a topic. Nyni se jeho velikost pres 545px nebude tak rapidne zvetsovat

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,13 +7,11 @@
         @if(isset($user))
             {{--<p><?php dd($categories); ?></p>--}}
             <div class="cat-main-div" onclick="resetEnter()">
-                <div class="head-title text-center">
+                <div class="head-title text-center cat-main-topic">
                     <h1>choose a topic</h1>
                 </div>
-
                 <div class="d-flex flex-wrap justify-content-around">
                     @for($k = 0; $k < $count;$k++)
-
                         {{--                        <div class="d-flex cat-col-md-2">--}}
                         <div class="d-flex col-sm-12 col-md-6 col-lg-4 col-xl-3">
                             <a href="{{ url('/category/' . $categoryNames[$k]->id) }}"
@@ -21,7 +19,6 @@
                                 {{$categoryNames[$k]->nameCZ}}
                             </a>
                         </div>
-
                     @endfor
                 </div>
             </div>
